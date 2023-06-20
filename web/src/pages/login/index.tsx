144b0a79037ef4fd5b2b88c7c09d2ce229eab82c
Icaro Ramos <polosnramos@hotmail.com>
2023-06-20 05:55:18 -0300

    feat: page Login

---
+++ web/src/pages/login/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import {
+  LoginContainer,
+  LoginForm,
+  Title,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from "./styles";
+import { Redirect } from "react-router-dom";
+const Login = () => {
+  const [userId, setUserId] = useState(null);
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const emailLogin = email;
+    try {
+      const response = await fetch(
+        `http://192.168.3.68:3333/users/${emailLogin}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        console.log("Login bem-sucedido!");
+        const userData = await response.json();
+        const { id } = userData;
+        setUserId(id);
+      } else {
+        console.log("Login falhou!");
+      }
+    } catch (error) {
+      console.log("Erro ao fazer login:", error);
+    }
+
+    setEmail("");
+  };
+
+  if (userId) {
+    return <Redirect to={`/store/${userId}`} />;
+  }
+
+  return (
+    <LoginContainer>
+      <LoginForm onSubmit={handleSubmit}>
+        <Title>Login</Title>
+        <FormGroup>
+          <Label htmlFor="email">Email:</Label>
+          <Input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </FormGroup>
+        <Button type="submit">Entrar</Button>
+      </LoginForm>
+    </LoginContainer>
+  );
+};
+
+export default Login;
---
+++ web/src/pages/login/styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const LoginForm = styled.form`
+  width: 40%;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f5f5f5;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const FormGroup = styled.div`
+  margin-bottom: 20px;
+  padding: 20px;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
