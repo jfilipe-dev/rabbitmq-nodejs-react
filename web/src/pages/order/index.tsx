6c24d407ab064c3af5f6b01572928e8285c81ae8
Icaro Ramos <polosnramos@hotmail.com>
2023-06-20 18:15:45 -0300

    feat: Order page

--- web/src/pages/order/index.tsx
+++ web/src/pages/order/index.tsx
@@ -1,41 +1,91 @@
 import React, { useEffect, useState } from "react";
-import { StatusContainer, StatusMessage, getStatusColor } from "./styles";
+import {
+  StatusContainer,
+  StatusMessage,
+  StyledOrders,
+  ProductName,
+  ProductId,
+  Price,
+  Quantity,
+  Button,
+  TitleMessage,
+} from "./styles";
+import { useParams } from "react-router-dom";
 
-export type OrderProps = {
+type OrdersProps = {
+  id: string;
+  items: {
+    product: {
+      id: string;
+      name: string;
+      price: number;
+    };
+    quantity: number;
+    subtotal: number;
+  }[];
+  total: number;
   status: string;
+  user: {
+    id: string;
+    email: string;
+  };
 };
 
-const Order = ({ status }: OrderProps) => {
-  const [order, setOrder] = useState<any>(null);
-  const color = getStatusColor(status);
+const Order = () => {
+  const { id, buyId } = useParams<any>();
+
+  const [orders, setOrders] = useState<OrdersProps>();
+
+  const [userData, setUserData] = useState<OrdersProps>();
 
   useEffect(() => {
-    const fetchOrder = async () => {
-      try {
-        const response = await fetch("URL_DA_API");
-        if (response.ok) {
-          const data = await response.json();
-          setOrder(data);
-        } else {
-          console.log("Erro ao obter o pedido");
-        }
-      } catch (error) {
-        console.log("Erro ao obter o pedido:", error);
-      }
-    };
-
-    fetchOrder();
+    const userDataString = localStorage.getItem("userData");
+    if (userDataString) {
+      setUserData(JSON.parse(userDataString));
+    }
   }, []);
 
-  if (!order) {
+  if (!userData) {
     return <div>Carregando...</div>;
   }
 
+  const fetchOrder = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.3.68:3333/orders/${id}/${buyId}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
+        localStorage.clear();
+      } else {
+        console.log("Erro ao obter o pedido");
+      }
+    } catch (error) {
+      console.log("Erro ao obter o pedido:", error);
+    }
+  };
+
   return (
     <StatusContainer>
-      <StatusMessage color={color}>Status do Pedido: {status}</StatusMessage>
-      <div>ID do Pedido: {order.id}</div>
-      {/* Restante das informações do pedido */}
+      <TitleMessage color={userData.status}>Resumo do Pedido:</TitleMessage>
+      <div>ID do Pedido: {userData.id}</div>
+      <StatusMessage color={userData.status}>
+        Status do Pedido: {userData.status}
+      </StatusMessage>
+      <div>
+        {userData.items.map((order: any, index: number) => (
+          <StyledOrders key={index}>
+            <ProductName>Produto: {order.product.name}</ProductName>
+            <ProductId>ID do Produto: {order.product.id}</ProductId>
+            <Price>Preço: R$ {order.product.price}</Price>
+            <Quantity>Quantidade: {order.quantity}</Quantity>
+            <Quantity>SubTotal: R${order.subtotal}</Quantity>
+          </StyledOrders>
+        ))}
+      </div>
+      <div>Tota: R${userData?.total}</div>
+      <Button onClick={fetchOrder}>Atualizar Status</Button>
     </StatusContainer>
   );
 };
